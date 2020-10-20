4e6874daf5890c054ee987e70d92909eca5c5cd7
YuvalWein <65113623+YuvalWein@users.noreply.github.com>
2020-10-20 23:17:06 +0300

    Bug Fix-ATR
    The function always calculated the ATR with EMA.
    Changed documentation to SMA as default.

--- pandas_ta/volatility/atr.py
+++ pandas_ta/volatility/atr.py
@@ -3,47 +3,42 @@ from pandas_ta.overlap import ema, rma
 from .true_range import true_range
 from pandas_ta.utils import get_drift, get_offset, verify_series
 
-
-def atr(high, low, close, length=None, mamode=None, drift=None, offset=None, **kwargs):
+def atr(high, low, close, length=None, mamode='sma', drift=None, offset=None, **kwargs):
     """Indicator: Average True Range (ATR)"""
+
     # Validate arguments
     high = verify_series(high)
     low = verify_series(low)
     close = verify_series(close)
     length = int(length) if length and length > 0 else 14
-    mamode = mamode.lower() if mamode else "ema"
+    min_periods = int(kwargs['min_periods']) if 'min_periods' in kwargs and kwargs['min_periods'] is not None else length
     drift = get_drift(drift)
     offset = get_offset(offset)
 
     # Calculate Result
     tr = true_range(high=high, low=low, close=close, drift=drift)
-    if mamode == "ema":
-        # alpha = (1.0 / length) if length > 0 else 0.5
-        # atr = tr.ewm(alpha=alpha).mean()
-        atr = rma(tr, length=length)
+    if mamode == 'ema':
+        alpha = (1.0 / length) if length > 0 else 0.5
+        atr = tr.ewm(alpha=alpha, min_periods=min_periods).mean()
     else:
-        # atr = tr.rolling(length).mean()
-        atr = sma(tr, length=length)
-
-    percentage = kwargs.pop("percent", False)
-    if percentage:
-        atr *= 100 / close
+        atr = tr.rolling(length, min_periods=min_periods).mean()
 
     # Offset
     if offset != 0:
         atr = atr.shift(offset)
 
     # Handle fills
-    if "fillna" in kwargs:
-        atr.fillna(kwargs["fillna"], inplace=True)
-    if "fill_method" in kwargs:
-        atr.fillna(method=kwargs["fill_method"], inplace=True)
+    if 'fillna' in kwargs:
+        atr.fillna(kwargs['fillna'], inplace=True)
+    if 'fill_method' in kwargs:
+        atr.fillna(method=kwargs['fill_method'], inplace=True)
 
     # Name and Categorize it
-    atr.name = f"ATR{'p' if percentage else ''}_{length}"
-    atr.category = "volatility"
+    atr.name = f"ATR_{length}"
+    atr.category = 'volatility'
 
     return atr
+
 
 
 atr.__doc__ = \
@@ -57,7 +52,7 @@ Sources:
 
 Calculation:
     Default Inputs:
-        length=14, drift=1, percent=False
+        length=14, drift=1
     SMA = Simple Moving Average
     EMA = Exponential Moving Average
     TR = True Range
@@ -67,22 +62,19 @@ Calculation:
     else:
         ATR = SMA(tr, length)
 
-    if percent:
-        ATR *= 100 / close
-
 Args:
     high (pd.Series): Series of 'high's
     low (pd.Series): Series of 'low's
     close (pd.Series): Series of 'close's
-    length (int): It's period. Default: 14
-    mamode (str): Two options: None or 'ema'. Default: 'ema'
-    drift (int): The difference period. Default: 1
-    offset (int): How many periods to offset the result. Default: 0
+    length (int): It's period.  Default: 14
+    mamode (str): Two options: 'sma' or 'ema'.  Default: 'sma'
+    drift (int): The difference period.   Default: 1
+    offset (int): How many periods to offset the result.  Default: 0
 
 Kwargs:
-    percent (bool, optional): Return as percentage. Default: False
     fillna (value, optional): pd.DataFrame.fillna(value)
     fill_method (value, optional): Type of fill method
+    min_periods (int, optional) : Minimum number of periods before calculating ATR. Default : length
 
 Returns:
     pd.Series: New feature generated.
